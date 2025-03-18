71cc55ec75d73d573692c41ae5af6f5154b5f6bb
CMS37 <rhtn037@naver.com>
2025-03-18 15:07:36 +0900

    주소 정보 추출

--- Code.js
+++ Code.js
@@ -1,3 +1,29 @@
 function myFunction() {
-  
+	var ss = SpreadsheetApp.getActiveSpreadsheet();
+	var sheet = ss.getActiveSheet();
+
+	var lastRow = sheet.getLastRow();
+
+	var dataRange = sheet.getRange(2, 1, lastRow - 1, sheet.getLastColumn());
+	var data = dataRange.getValues();
+
+	var addresses = [];
+
+	for (var i = 0; i < data.length; i++) {
+		var row = data[i];
+		var streetAddress = row[9];
+		var city = row[12];
+		var state = row[13];
+		var postalCode = row[15];
+
+		addresses.push({
+			street: streetAddress,
+			city: city,
+			state: state,
+			postalCode: postalCode
+		});
+	}
+
+	Logger.log(addresses);
 }
+  
